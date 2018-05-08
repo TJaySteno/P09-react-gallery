243f0baffd86dd6c62e4f15e19c40a9930196936
Trent <trent.stenoien@gmail.com>
2018-05-08 16:39:25 -0500

    Comments

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
-// only need Route if you need match in App.js
-  // if you don't need Route in App.js, you can likely do BrowserRouter there as well
 
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
+// Initialize React Router and use Route to track its match, location, and history props
 ReactDOM.render(<BrowserRouter><Route component={App} /></BrowserRouter>,
   document.getElementById('root'));
 

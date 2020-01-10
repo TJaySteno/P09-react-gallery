b636a2a7f924277cda6879a4b5d91b1f255211c0
Trent <trent.stenoien@gmail.com>
2020-01-09 21:47:15 -0600

    New error picture

--- src/components/Error.js
+++ src/components/Error.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Loading = () =>
   <ul>
     <li className='error'>
-      <img src='https://c1.staticflickr.com/7/6136/5930381013_953cce35f5_q.jpg' alt='Technology monkey' className='not-found-monkey' />
+      <img src='https://live.staticflickr.com/3699/12923965803_5b1ca8f081.jpg' alt='Technology monkey' className='not-found-monkey' />
       <h3>Page Not Found (404)</h3>
       <p>Our monkeys can't seem to find the page you want. Please try again.</p>
     </li>
